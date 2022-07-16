755c507bd84cff1d010b6ae3d67c696660ccfed9
ZhukNikita <nikitazhuk99@icloud.com>
2022-07-16 11:43:48 +0300

    new commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,39 +9,49 @@ export default function App(){
 	let month = date.getMonth() + 1
 	const [trackId, setTrackId] = useState(0)
 	const [tracks, setNewTracks] = useState([])	  
-	const [trackName , setTrackName] = useState('')
+	const [trackName , setTrackName] = useState(`${date.getDate()} / ${month.toString().padStart(2, '0')} / ${date.getFullYear()}`)
 	function TrackName(e){
       	setTrackName(e.target.value)
       	if(!e.target.value){
-      		setTrackName([date.getDate() , month.toString().padStart(2, '0') , date.getFullYear()].join('/'))
+      		setTrackName(`${date.getDate()}/${month.toString().padStart(2, '0')}/${date.getFullYear()}`)
       	}
   	}
-  	const track = {
+  function handleKeyDown(e){
+  	if (e.key === 'Enter') {
+    setTrackId(trackId + 1)
+    setNewTracks([...tracks , track])
+    e.target.value = ''
+	  if(!e.target.value){
+	      		setTrackName(`${date.getDate()}/${month.toString().padStart(2, '0')}/${date.getFullYear()}`)
+	  }
+		}
+  }
+  const track = {
   id: trackId ,
   value : 	<div>
          		<h4>{trackName}</h4>
         	</div>
     }
-  function CreateNewTrack(e){
-  	if (e.key === 'Enter') {
+  function CreateNewTrack(){
     setTrackId(trackId + 1)
     setNewTracks([...tracks , track])
-	}
   }
     const TrackList = tracks.map((track)=>
     <li key={track.id}>
       {track.value}
     </li>
-  )
+  )	
 
 	return(
 	    <div className="App">
 	      <div className="tracker">
 	        <h1>Tracker</h1>
-	        <AddTrack TrackName = {TrackName} CreateNewTrack={CreateNewTrack} />
+	        <AddTrack TrackName = {TrackName} CreateNewTrack={CreateNewTrack} handleKeyDown={handleKeyDown}/>
 	      </div>
 	      <div className="trackList">
-	      	{TrackList}
+	      	<ul>
+	      		{TrackList}
+	      	</ul>
 	      </div>
 	    </div>
 	)
