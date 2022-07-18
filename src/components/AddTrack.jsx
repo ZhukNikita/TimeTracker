7f499161303e17955158e12da999d403c0897e9d
ZhukNikita <nikitazhuk99@icloud.com>
2022-07-18 17:40:32 +0300

    new commit

--- src/components/AddTrack.jsx
+++ src/components/AddTrack.jsx
@@ -5,8 +5,14 @@ import React from 'react';
 function AddTrack(props) {
   return (
     <div className="AddTrack">
-        <input type="text" placeholder="Enter track name" onChange={props.TrackName}  onKeyDown={props.CreateNewTrack}/>
-        <button onClick={props.CreateNewTrack}>
+        <input 
+          type="text"
+          placeholder="Enter tracker name"
+          onChange={props.TrackName} 
+          onKeyDown={props.handleKeyDown}
+          maxLength="25"
+        />
+        <button type="reset" onClick={props.CreateNewTrack}>
           <span className="material-icons">
             play_circle
           </span>
